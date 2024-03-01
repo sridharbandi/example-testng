923f4bb07f0183727177019b6dff006a40e50c78
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2024-03-01 10:59:14 +0000

    Selenium

--- src/test/java/org/test/RunTest.java
+++ src/test/java/org/test/RunTest.java
@@ -1,24 +1,48 @@
 package org.test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class RunTest {
 
+    WebDriver driver;
     @BeforeMethod
     public void preCondition(){
-        System.out.println("Pre-condition");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
     }
 
     @Test
     public void testOne(){
-        System.out.println("Test One");
+        // Navigate to the Sauce Demo login page
+        driver.get("https://www.saucedemo.com/");
+
+        // Enter username and password
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        // Click on the login button
+        driver.findElement(By.id("login-button")).click();
+
+        // Verify that login is successful
+        String currentUrl = driver.getCurrentUrl();
+        assert currentUrl.equals("https://www.saucedemo.com/inventory.html") : "Login unsuccessful!";
+
     }
 
     @AfterMethod
     public void cleanUp(){
-        System.out.println("In clean up");
+        // Close the browser after test execution
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
 
